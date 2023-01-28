fdc81471d021cda378c79b2dcc75eb19fce44ecd
KW <kamil.wojtasinski.poczta@gmail.com>
2023-01-28 23:26:12 +0100

    [Module4] TDD and Code Coverage

--- TodoApp.UnitTests/Services/QuestServiceTests.cs
+++ TodoApp.UnitTests/Services/QuestServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
+using Shouldly;
 using TodoApp.Core.DTO;
 using TodoApp.Core.Entities;
+using TodoApp.Core.Exceptions;
 using TodoApp.Core.Repositories;
 using TodoApp.Core.Services;
 
@@ -16,6 +18,52 @@ namespace TodoApp.UnitTests.Services
             _questService.AddQuest(dto);
 
             _repository.Verify(r => r.Add(It.IsAny<Quest>()), times: Times.Once);
+        }
+
+        [Fact]
+        public void should_quest_status()
+        {
+            var quest = CreateDefaultQuest();
+            _repository.Setup(r => r.Get(quest.Id)).Returns(quest);
+            var status = QuestStatus.Complete.ToString();
+
+            var questAfterUpdate = _questService.ChangeQuestStatus(quest.Id, status);
+
+            questAfterUpdate.ShouldNotBeNull();
+            _repository.Verify(r => r.Update(quest));
+            questAfterUpdate.Status.ShouldBe(status);
+        }
+
+        [Fact]
+        public void given_invalid_id_when_change_quest_status_should_throw_an_exception()
+        {
+            var id = 1;
+            var status = "avbc";
+
+            var exception = Record.Exception(() => _questService.ChangeQuestStatus(id, status));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<CustomException>();
+            exception.Message.ShouldContain("not found");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("      ")]
+        [InlineData("nie+istnieje")]
+        [InlineData("200")]
+        public void given_invalid_status_when_change_quest_status_should_throw_an_exception(string status)
+        {
+            var quest = CreateDefaultQuest();
+            _repository.Setup(r => r.Get(quest.Id)).Returns(quest);
+            var expectedException = new CustomException($"There is no Quest status {status}");
+
+            var exception = Record.Exception(() => _questService.ChangeQuestStatus(quest.Id, status));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType(expectedException.GetType());
+            exception.Message.ShouldBe(expectedException.Message);
         }
 
         public static Quest CreateDefaultQuest(int id = 1, string? title = null, string? description = null, QuestStatus questStatus = QuestStatus.New)
