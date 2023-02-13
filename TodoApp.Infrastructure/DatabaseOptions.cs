4fedb97300306fffae6697befaf0842856588c77
KW <kamil.wojtasinski.poczta@gmail.com>
2023-02-13 19:31:32 +0100

    [Module7] EF Core Creating Enitites, Migrations

--- TodoApp.Infrastructure/DatabaseOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TodoApp.Infrastructure
-{
-    public class DatabaseOptions
-    {
-        public string? ConnectionString { get; set; }
-        public bool AllowMigrations { get; set; }
-    }
-}
--- TodoApp.Infrastructure/Extensions.cs
+++ TodoApp.Infrastructure/Extensions.cs
@@ -1,13 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using MySql.Data.MySqlClient;
-using System.Data;
-using System.Text.Json;
-using TodoApp.Core.Entities;
-using TodoApp.Core.Repositories;
 using TodoApp.Infrastructure.Database;
-using TodoApp.Infrastructure.Repositories;
-using TodoApp.Infrastructure.Repositories.Files;
-using TodoApp.Migrations;
 
 namespace TodoApp.Infrastructure
 {
@@ -15,12 +7,7 @@ namespace TodoApp.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, Dictionary<string, object> appsettings)
         {
-            //services.AddSingleton(typeof(IRepository<>), typeof(Repository<>));
-            services.AddScoped(typeof(IRepository<>), typeof(JsonFileRepository<>));
-            services.AddScoped<IRepository<Quest>, QuestRepository>();
             services.AddDatabase(appsettings);
-            services.AddSingleton(typeof(IPrimaryKeyManager<>), typeof(PrimaryKeyManager<>));
-            services.AddSingleton(typeof(IPrimaryKeyPositionCache<>), typeof(PrimaryKeyPositionCache<>));
             return services;
         }
 
@@ -29,33 +16,6 @@ namespace TodoApp.Infrastructure
             var dbInitializer = serviceProvider.GetRequiredService<IDbInitializer>();
             dbInitializer.Start();
             return serviceProvider;
-        }
-
-        private static IServiceCollection AddDatabase(this IServiceCollection services, Dictionary<string, object> appsettings)
-        {
-            var databaseOptionsJsonElement = (JsonElement)(appsettings["database"]
-                ?? throw new InvalidOperationException("Cannot find section 'database', please ensure that this file exists"));
-            var databaseOptions = databaseOptionsJsonElement.Deserialize<DatabaseOptions>(new JsonSerializerOptions()
-            {
-                PropertyNameCaseInsensitive = true,
-            }) ?? throw new InvalidOperationException("Cannot deserialize section 'database', please ensure that this section is correct");
-            if (string.IsNullOrWhiteSpace(databaseOptions.ConnectionString))
-            {
-                throw new InvalidOperationException("Check in 'database' section 'connectionString' if is correct");
-            }
-            services.AddSingleton(databaseOptions);
-            services.AddTransient<IDbInitializer, DbInitializer>();
-
-            if (databaseOptions.AllowMigrations)
-            {
-                services.AddMigrations(databaseOptions.ConnectionString);
-            }
-
-            services.AddScoped<IDbConnection, MySqlConnection>(sp =>
-            {
-                return new MySqlConnection(databaseOptions.ConnectionString);
-            });
-            return services;
         }
     }
 }
