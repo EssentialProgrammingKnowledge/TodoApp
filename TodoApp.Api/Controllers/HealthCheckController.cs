e9031a6e927f05e24594489dbe25de53e2511fe4
KW <kamil.wojtasinski.poczta@gmail.com>
2023-02-16 22:22:57 +0100

    [Module8] Controller

---
+++ TodoApp.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "TodoApp!";
+        }
+    }
+}
